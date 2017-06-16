940fe398b09e8dafd82e1b5962d2d662f0e1bfcd
Xing <xli227@uic.edu>
2017-06-16 13:41:27 -0400

    added configuration settings and JSON checks

--- src/extension.ts
+++ src/extension.ts
@@ -7,24 +7,30 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+    function checkJSON(text) {
+        if (text === "") {
+            vscode.window.showErrorMessage("No text selected");
+        }
+        try {
+            JSON.parse(text);
+        }
+        catch (e) {
+            vscode.window.showErrorMessage("Not a JSON:" + e);
+        }
+    }
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    /*let vscode = require('vscode');
-    let path = require("path");
-    let currentlyOpenTabfile = vscode.window.activeTextEditor.document
-    */
-   
-    let index = '/Users/xingli/Documents/UIC/Research/kasimvizVSCode/src/viz/index.html'
     
-    //let index = '/Users/xingli/Documents/UIC/Research/KaSimVSExtension/kasim/src/viz/index.html'
+    let index = vscode.workspace.getConfiguration('kasimviz')['path'];
+    
+        //let index = '/Users/xingli/Documents/UIC/Research/KaSimVSExtension/kasim/src/viz/index.html'
     let filePath = vscode.Uri.file(index);
-    
     //let registration = vscode.workspace.registerTextDocumentContentProvider('css-preview', provider);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.kasimviz', () => {
-        // The code you place here will be executed every time your command is execute
+    // The code you place here will be executed every time your command is execute
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -32,11 +38,15 @@ export function activate(context: vscode.ExtensionContext) {
 
         let fs = require('fs');
         let text = fs.readFileSync(index, 'utf8');
+
+        
         // Display a message box to the user
         let selection = editor.selection;
         let data = editor.document.getText(selection);
+        checkJSON(data);
         if(data !== "") {
-            text = text.replace(/setData(.*)/, 'setData(' + data.toString() + ');');
+            data = data.toString().replace(/\r?\n/g, " ");
+            text = text.replace(/setData\(.*\)\;/, 'setData(' + data.toString() + ');');
             fs.writeFileSync(index, text, 'utf8');
         }
 
